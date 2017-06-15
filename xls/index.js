f95f2e216243ebf0072b5b58c2e9f6b72a1135c2
mgechev <mgechev@gmail.com>
2017-06-15 14:09:27 -0700

    feat: add report xlsx generation

---
+++ xls/index.js
@@ -0,0 +1,43 @@
+const xlsx = require('node-xlsx');
+const fs = require('fs');
+const data = require('../scraper/all.json');
+
+function processSubcategory(subCategory) {
+  const subCategoryName = subCategory.title;
+  return subCategory.products.map(p => ([
+    subCategoryName, p.title, p.price, parseInt(p.sales) || 5, p.brand, p.image ]));
+}
+
+function processSheet(category) {
+  const data = [['Sub-category', 'Product', 'Price', 'Est. sales', 'Brand', 'Image']];
+  return category.subCategories.reduce((p, subCategory) =>
+    p.concat(processSubcategory(subCategory)), data);
+}
+
+const usedNames = {};
+function getValidName(name) {
+  name = name.replace(/[^\w\d]/g, '');
+  let i = 1;
+  let cName = name;
+  while (usedNames[cName] && i < 100) {
+    cName = name + ' ' + i.toString();
+    i += 1;
+  }
+  if (i >= 100) {
+    console.error('Invalid name');
+    process.exit(1);
+  }
+  console.log(cName);
+  usedNames[cName] = true;
+  return cName;
+}
+
+function processSheets(categories) {
+  return categories
+    .filter(c => !!c.subCategories)
+    .map(c => ({ name: getValidName(c.title), data: processSheet(c) }));
+}
+
+var buffer = xlsx.build(processSheets(data));
+
+fs.writeFileSync('report.xlsx', buffer);
